5424282c234740a0ad7b0c4c9915815d8458f263
Anil Ustundag <ustundaganil@gmail.com>
2021-03-05 17:05:29 +0300

    change popup title for each case.

--- src/main/kotlin/org/bilalkilic/kediatrhelper/utils/PopupConstants.kt
+++ src/main/kotlin/org/bilalkilic/kediatrhelper/utils/PopupConstants.kt
@@ -2,8 +2,9 @@ package org.bilalkilic.kediatrhelper.utils
 
 object PopupConstants {
 
-    const val POPUP_TITLE_PREFIX_NEW_HANDLER = "No Handler Found For "
-    const val POPUP_TITLE_PREFIX_MULTIPLE_HANDLERS = "More Than 1 Handler Found For "
+    const val POPUP_TITLE_PREFIX_FOR_NO_HANDLER = "No Handler Found For "
+    const val POPUP_TITLE_PREFIX_FOR_ONE_HANDLER = "Only 1 Handler Found For "
+    const val POPUP_TITLE_PREFIX_FOR_MULTIPLE_HANDLERS = "More Than 1 Handler Found For "
     const val POPUP_STEP_PREFIX_NAVIGATE = "Navigate to "
     const val POPUP_STEP_HANDLER_BASIC = "Create New Handler... "
     const val POPUP_STEP_HANDLER_ASYNC = "Create New Async Handler... "
